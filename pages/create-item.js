712d4bf654fb0d912ebeb2426c057c3b74838bcd
Julian <perassijulian@gmail.com>
2022-06-05 20:27:08 -0300

    create item page

--- pages/create-item.js
+++ pages/create-item.js
@@ -2,14 +2,16 @@ import { ethers } from "ethers";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { create as ipfsHttpClient } from "ipfs-http-client";
+import Web3Modal from "web3modal";
 
 import { marketplaceAddress } from "../config";
+import NFTMarketplace from "../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json";
 
 const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
 
 export default function CreateItem() {
   const [fileUrl, setFileUrl] = useState(null);
-  const [formData, setFormData] = useState({
+  const [formInput, setFormInput] = useState({
     price: "",
     name: "",
     description: "",
@@ -48,4 +50,67 @@ export default function CreateItem() {
       console.log("Error uploading file: ", error);
     }
   }
+
+  async function listNFTForSale() {
+    const url = await uploadToIPFS();
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+
+    /* create the NFT */
+    const price = ethers.utils.parseUnits(formInput.price, "ether");
+
+    let contract = new ethers.Contract(
+      marketplaceAddress,
+      NFTMarketplace.abi,
+      signer
+    );
+
+    let listingPrice = await contract.getListingPrice();
+    listingPrice = listingPrice.toString();
+    let transaction = await contract.createToken(url, price, {
+      value: listingPrice,
+    });
+    await transaction.wait();
+    console.log(transaction)
+
+    router.push("/");
+  }
+
+  return (
+    <div className="flex justify-center">
+      <div className="w-1/2 flex flex-col pb-12">
+        <input
+          placeholder="Asset Name"
+          className="mt-8 border rounded p-4"
+          onChange={(e) =>
+            setFormInput({ ...formInput, name: e.target.value })
+          }
+        />
+        <textarea
+          placeholder="Asset Description"
+          className="mt-2 border rounded p-4"
+          onChange={(e) =>
+            setFormInput({ ...formInput, description: e.target.value })
+          }
+        />
+        <input
+          placeholder="Asset Price in Eth"
+          className="mt-2 border rounded p-4"
+          onChange={(e) =>
+            setFormInput({ ...formInput, price: e.target.value })
+          }
+        />
+        <input type="file" name="Asset" className="my-4" onChange={onChange} />
+        {fileUrl && <img className="rounded mt-4" width="350" src={fileUrl} />}
+        <button
+          onClick={listNFTForSale}
+          className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg"
+        >
+          Create NFT
+        </button>
+      </div>
+    </div>
+  );
 }
