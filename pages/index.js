90c98dc60ba7bf96390423c31a08a1b5ccf184c4
Julian <perassijulian@gmail.com>
2022-06-05 20:39:37 -0300

    UI home

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
   useEffect(() => {
     loadNFTs();
   }, []);
-  
+
   async function loadNFTs() {
     const provider = new ethers.providers.JsonRpcProvider();
     const contract = new ethers.Contract(
@@ -69,5 +69,29 @@ export default function Home() {
   if (loadingState == "loaded" && !nfts.length)
     return <h1 className="px-20 py-10 text-3xl">No items found</h1>;
 
-  return <div className="flex justify-center"></div>;
+  return (
+    <div className="flex justify-center">
+      <div className="px-4" style={{ maxWidth: "1600px" }}>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+        {
+          nfts.map((nft, i) => (
+            <div key={i} className="border shadow rounded-xl overflow-hidden">
+              <img src={nft.image} style={{maxHeight:'300px'}}/>
+              <div className="p-4">
+                <p style={{ height: '64px' }} className="text-2xl font-semibold">{nft.name}</p>
+                <div style={{ height: '70px', overflow: 'hidden' }}>
+                  <p className="text-gray-400">{nft.description}</p>
+                </div>
+              </div>
+              <div className="p-4 bg-black">
+                <p className="text-2xl font-bold text-white">{nft.price} ETH</p>
+                <button className="mt-4 w-full bg-pink-500 text-white font-bold py-2 px-12 rounded" onClick={() => buyNft(nft)}>Buy</button>
+              </div>
+            </div>
+          ))
+        }
+        </div>
+      </div>
+    </div>
+  );
 }
