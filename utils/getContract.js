840381b3f7bfdda567bc8bdf1019f30e4d01fef4
Julian <perassijulian@gmail.com>
2022-06-14 21:25:53 -0300

    back to regular conection

--- utils/getContract.js
+++ utils/getContract.js
@@ -16,17 +16,10 @@ export const getContractSigned = async () => {
 
 export const getContractPublic = async () => {
   //DEPLOYING FOR MUMBAI
-  // const address = marketplaceAddresses["80001"][0];
-  // const provider = new ethers.providers.JsonRpcProvider(
-  //   process.env.NEXT_PUBLIC_API_URL_MUMBAI
-  // );
-  // const contract = new ethers.Contract(address, abi, provider);
-  // return contract;
-  const web3Modal = new Web3Modal();
-  const connection = await web3Modal.connect();
-  const provider = new ethers.providers.Web3Provider(connection);
   const address = marketplaceAddresses["80001"][0];
-  let contract = new ethers.Contract(address, abi, provider);
+  const provider = new ethers.providers.JsonRpcProvider(
+    process.env.NEXT_PUBLIC_API_URL_MUMBAI
+  );
+  const contract = new ethers.Contract(address, abi, provider);
   return contract;
-
 };
